47240fe4fedde4f49b406def554398fcc1ffba87
DOME MIE <domelaz2556@gmail.com>
2022-02-09 22:48:48 +0700

    Add database to App

--- config/database.php
+++ config/database.php
@@ -65,13 +65,13 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('postgres://nvncwtzjdsixxo:3d6308aae105438025ec262bfa5b1114248df44f5a9f6ad36c6ec3dbfca6ebbc@ec2-3-219-204-29.compute-1.amazonaws.com:5432/d6daet5aletggv'),
+            'url' => env('postgres://fqiqzbojwolomg:b7177cba66807d8a4c0e24fff60d2e5d1f6d1355321f4795919f8556f9f06e07@ec2-52-207-74-100.compute-1.amazonaws.com:5432/dbth3o4m6l6l9n'),
             'host' => env('DB_HOST', '
-            ec2-3-219-204-29.compute-1.amazonaws.com'),
+            ec2-52-207-74-100.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd6daet5aletggv'),
-            'username' => env('DB_USERNAME', 'nvncwtzjdsixxo'),
-            'password' => env('DB_PASSWORD', '3d6308aae105438025ec262bfa5b1114248df44f5a9f6ad36c6ec3dbfca6ebbc'),
+            'database' => env('DB_DATABASE', 'dbth3o4m6l6l9n'),
+            'username' => env('DB_USERNAME', 'fqiqzbojwolomg'),
+            'password' => env('DB_PASSWORD', 'b7177cba66807d8a4c0e24fff60d2e5d1f6d1355321f4795919f8556f9f06e07'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
